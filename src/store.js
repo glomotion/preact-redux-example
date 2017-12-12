c0e1c388e260ef487df8124e9528b5f95e28f5a6
Tim Paul <tim@grumpysailor.com>
2017-12-12 11:28:58 +1100

    simplify examples

--- src/store.js
+++ src/store.js
@@ -1,18 +1,18 @@
 import { createStore } from 'redux';
 
 let ACTIONS = {
-	ADD_TODO: ({ todos, state }, { text }) => ({
+	ADD_TODO: ({ todos, state }, { text }) => {
 		todos: [...todos, {
 			id: Math.random().toString(36).substring(2),
 			text
 		}],
 		...state
-	}),
+	},
 
-	REMOVE_TODO: ({ todos, state }, { todo }) => ({
+	REMOVE_TODO: ({ todos, state }, { todo }) => {
 		todos: todos.filter( i => i !== todo ),
 		...state
-	}),
+	},
 
 
 	//             { previous state }     { action }
